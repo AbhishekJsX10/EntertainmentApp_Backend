f75420b7512609f73f0f682b81fa6c8f7f9e7f2a
Abhishek10usedit <avishekchaturvedi0@gmail.com>
2024-05-02 02:01:58 +0530

    first commit

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ import express from "express"
 import mongoose from "mongoose"
 import cookieParser from "cookie-parser"
 import cors from "cors"
-import dotenv from "dotenv"
+import { config } from 'dotenv';
 
-dotenv.config()
-const app = express()
 
 import userRouter from "./src/routes/userRoutes.js"
 import tvShowsRouter from "./src/routes/tvShowsRoutes.js"
@@ -16,17 +14,25 @@ import bookmarkRouter from "./src/routes/bookmarkRoutes.js"
 import recommendRouter from "./src/routes/recommendRoutes.js"
 
 
-
-
-
-mongoose.connect(process.env.MONGO_DB_URL,{
-    dbName:"Entertainment_App"
-}).then(()=>console.log("MongoDB database is connected"))
+const app = express()
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
 
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
+
+config({
+    path:'./.env'
+})
+
+mongoose.connect(process.env.MONGO_DB_URL,{
+    dbName:"Entertainment_App"
+}).then(()=>console.log("MongoDB database is connected"))
 
 app.use("/user",userRouter)
 app.use("/tvshow",tvShowsRouter)
@@ -35,11 +41,7 @@ app.use("/recommends",recommendRouter)
 app.use("/movies",moviesRouter)
 app.use("/bookmarks",bookmarkRouter)
 
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true
-}))
+
 
 app.get("/",async(req,res)=>{
     res.send("hello")
